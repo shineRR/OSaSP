43e2561d483d3a756c18bbc81005e557d4119153
Ilya Baryko <38408304+shineRR@users.noreply.github.com>
2020-09-16 01:54:15 +0300

    Added moving object

--- L1_5Sem/main.cpp
+++ L1_5Sem/main.cpp
@@ -1,9 +1,14 @@
 #include <windows.h>
-#include <iostream>
+#include <gdiplus.h>
+
+using namespace Gdiplus;
+
+#define TIMER_SECOND 1
 
 SIZE MIN_WINDOW_SIZE = {300,200};
-int MOVE_DISTANCE = 10;
-int RECT_SIZE = 50;
+int MOVE_DISTANCE = 12;
+POINTFLOAT modifierDirection = {1.0, 1.0};
+int RECT_SIZE = 150;
 RECT rect = {0, 0, RECT_SIZE, RECT_SIZE};
 
 void updateRectValues(RECT &rectToChange, int horizontalPx, int verticalPx) {
@@ -15,9 +20,7 @@ void updateRectValues(RECT &rectToChange, int horizontalPx, int verticalPx) {
 
 bool calculateBorders(RECT windowRect, int horizontalPx, int verticalPx) {
     RECT tmpRect = rect;
-    std::cout << tmpRect.bottom << " before\n";
     updateRectValues(tmpRect, horizontalPx, verticalPx);
-    std::cout << tmpRect.bottom << " after\n";
     if (tmpRect.left < windowRect.left || tmpRect.top < windowRect.top || tmpRect.right > windowRect.right ||
         tmpRect.bottom > windowRect.bottom)
         return false;
@@ -34,10 +37,38 @@ void paintObj(HWND hWnd, int horizontalPx, int verticalPx) {
 
     PAINTSTRUCT ps;
     HDC hdc = BeginPaint(hWnd, &ps);
+    Graphics graphics(hdc);
+    Image image(L"C:\\Users\\shine\\Desktop\\Dev\\OSaSP\\L1_5Sem\\Assets\\apple.png");
+    Rect destRect(rect.left, rect.top, RECT_SIZE, RECT_SIZE);
+    graphics.DrawImage(&image, destRect);
     SelectObject(hdc, GetStockObject(DC_BRUSH));
-    SetDCBrushColor(hdc, RGB(200,100,0));
-    Rectangle(hdc, rect.left, rect.top, rect.right, rect.bottom);
     EndPaint(hWnd, &ps);
+}
+
+void iconMovement(HWND hWnd) {
+    RECT windowRect;
+    int xDistance = MOVE_DISTANCE * modifierDirection.x;
+    int yDistance = MOVE_DISTANCE * modifierDirection.y;
+    if (!GetClientRect(hWnd, &windowRect))
+        GetLastError();
+    if (windowRect.bottom == 0) return;
+    if (rect.bottom + yDistance > windowRect.bottom && rect.right + xDistance < windowRect.right &&
+            modifierDirection.y > 0) {
+        modifierDirection.y *= -1.0;
+    } else if (rect.bottom + yDistance > windowRect.bottom && rect.left + xDistance > windowRect.left
+            && modifierDirection.y > 0) {
+        modifierDirection.y *= -1.0;
+    } else if (rect.right + xDistance > windowRect.right && rect.bottom + yDistance <= windowRect.bottom &&
+               modifierDirection.x > 0) {
+        modifierDirection.x = -1.0;
+    } else if (rect.top + yDistance < windowRect.top && rect.left + xDistance > windowRect.left &&
+               modifierDirection.y < 0) {
+        modifierDirection.y *= -1.0;
+    } else if (rect.left + xDistance < windowRect.left && rect.top + yDistance >= windowRect.top &&
+               modifierDirection.x < 0) {
+        modifierDirection.x *= -1.0;
+    }
+    paintObj(hWnd,MOVE_DISTANCE * modifierDirection.x, MOVE_DISTANCE * modifierDirection.y);
 }
 
 void updateRectPos(HWND hWnd) {
@@ -106,15 +137,19 @@ void mouseWheelAnalyse(HWND hWnd, WPARAM wParam) {
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-    switch (message)
-    {
-        case WM_GETMINMAXINFO:
-        {
+    switch (message) {
+        case WM_CREATE:
+            SetTimer(hWnd, TIMER_SECOND, 200, NULL);
+            break;
+        case WM_GETMINMAXINFO: {
             updateRectPos(hWnd);
-            LPMINMAXINFO lpMMI = (LPMINMAXINFO)lParam;
+            LPMINMAXINFO lpMMI = (LPMINMAXINFO) lParam;
             lpMMI->ptMinTrackSize.x = MIN_WINDOW_SIZE.cx;
             lpMMI->ptMinTrackSize.y = MIN_WINDOW_SIZE.cy;
         }
+        case WM_TIMER:
+            iconMovement(hWnd);
+            break;
         case WM_KEYDOWN:
             keyDownAnalyse(hWnd, wParam);
             break;
@@ -136,6 +171,9 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow)
 {
+    GdiplusStartupInput gdiplusStartupInput;
+    ULONG_PTR gdiplusToken;
+    GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, nullptr);
     WNDCLASSEX wcex; HWND hWnd; MSG msg;
     wcex.cbSize = sizeof(WNDCLASSEX);
     wcex.style = CS_DBLCLKS;
@@ -160,5 +198,6 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdL
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
+    GdiplusShutdown(gdiplusToken);
     return (int)msg.wParam;
 }
